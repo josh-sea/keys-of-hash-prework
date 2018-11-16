b9e558ba8db89bf3acf207b90832a18774f51f98
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-16 20:08:34 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*arguments)
     # code goes here
 #    binding.pry
-    self.map do |key, value|
+    map do |key, value|
       if arguments.include?(value)
         key
       end
